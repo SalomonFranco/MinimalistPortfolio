048f03fb026d56d0261f6e2cd96e23e7f4c14cfd
Salomon Franco <salomonfrancodasilva@gmail.com>
2023-09-27 19:22:31 +0100

    updatingPersonalDetails

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -25,8 +25,8 @@ const Footer = ({}) => {
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         Made With ❤ by{" "}
-        <Link href="http://www.chetanverma.com">
-          <a className="underline underline-offset-1">Chetan Verma</a>
+        <Link href="http://www.jesicafranco.com">
+          <a className="underline underline-offset-1">Jesica Franco</a>
         </Link>
       </h1>
     </>
